88aa80b8422f398ba92fad02d0f1ac3bdd0b85be
Kunark Khewal <kunark.khewal@tothenew.com>
2021-01-09 18:48:54 +0530

    added progress bar for route change using NProgress

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,24 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = url => {
+    console.log(url)
+    NProgress.start();
+}
+
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 const Layout = ({children, title}) => (
     <div className="root">
+
+        <Head>
+            <title>NextPortfolio</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
+        </Head>
+
         <header>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
